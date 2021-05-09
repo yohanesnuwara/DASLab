08c9af7728075a6374fad937e086b40076d35662
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2021-05-09 18:53:43 +0700

    deactivate line 751

--- signalprocessing.py
+++ signalprocessing.py
@@ -747,19 +747,20 @@ def denoising_out(impactor2020,noise,ar_dep):
 def mycp(obj):
     import copy
     return copy.deepcopy(obj)
-def load_tdms_as_Fibers(outpath):
-    slx2020=Tdms()
-    slx2020.load_variables(outpath)
-    slx=Fibres()
-    slx.nsamples=mycp(slx2020.nt)
-    slx.nchannels=mycp(slx2020.nchannels)
-    slx.nt=mycp(slx2020.nt)
+
+# def load_tdms_as_Fibers(outpath):
+#     slx2020=Tdms()
+#     slx2020.load_variables(outpath)
+#     slx=Fibres()
+#     slx.nsamples=mycp(slx2020.nt)
+#     slx.nchannels=mycp(slx2020.nchannels)
+#     slx.nt=mycp(slx2020.nt)
 
 
-    slx.zz=mycp(slx2020.zz)
-    slx.data=mycp(slx2020.data)
-    slx.tt=mycp(slx2020.tt)
-    return slx
+#     slx.zz=mycp(slx2020.zz)
+#     slx.data=mycp(slx2020.data)
+#     slx.tt=mycp(slx2020.tt)
+#     return slx
 
 def load_segy_as_Fibers(segyfile,tdmsfile):
     slx2020=Tdms()
